55d0fb4ed58083b2d9a3bee6c199a5630f677fe0
beka-tech <abeka039@gmail.com>
2023-01-27 17:22:15 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import BlogList from './BlogList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Home  />
+       
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,34 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
-
   const [blogs, setBlogs] = useState([
-    {title:'My new website', body:'lorem ipsum...',author:'mario', id:1},
-    {title:'Welcome', body:'lorem ipsum...',author:'yoshi', id:2},
-    {title:'Web dev top tips', body:'lorem ipsum...',author:'mario', id:3},
-  ])
-  
-  
-
- 
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome my blog",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-     {blogs.map((blog ) => (
-      <div className="blog-preview" key={blog.id}>
-        <h2>{ blog.title }</h2>
-        <p>Written by { blog.author }</p>
+      <BlogList blogs={blogs} title="All blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Marios Blogs" />
 
-      </div>
-     ) )}
     </div>
   );
-}
- 
+};
+
 export default Home;
